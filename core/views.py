01b9a1848740bacdaee0e3a12f994dc78077f597
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2023-03-15 10:15:29 +0100

    Commit 7 Post feed

--- core/views.py
+++ core/views.py
@@ -14,8 +14,10 @@ def index(request):
     user_object = User.objects.get(username=request.user.username)
     user_profile = Profile.objects.get(user=user_object)
 
+    posts = Post.objects.all()
     return render(request, 'index.html', {
-        'user_profile': user_profile
+        'user_profile': user_profile,
+        'posts': posts
     })
 
 
