8909ce509f2f275a2d9358625a2a8dfecc23e03e
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2023-03-16 11:19:42 +0100

    Commit 13 User Suggestions

--- core/views.py
+++ core/views.py
@@ -1,5 +1,7 @@
 from itertools import chain
 from typing import List
+import random
+
 
 from django.contrib import messages, auth
 from django.contrib.auth.decorators import login_required
@@ -30,10 +32,37 @@ def index(request):
         feed.append(feed_list)
 
     feed_list = list(chain(*feed))
+
+    all_users = User.objects.all()
+    user_following_all = []
+
+    for user in user_following:
+        user_list = User.objects.get(username=user.user)
+        user_following_all.append(user_list)
+
+    new_suggestions_list = [x for x in list(all_users) if (x not in list(user_following))]
+    current_user = User.objects.filter(username=request.user.username)
+
+    final_suggestions_list = [x for x in list(new_suggestions_list) if (x not in list(current_user))]
+    random.shuffle(final_suggestions_list)
+
+    username_profile = []
+    username_profile_list = []
+
+    for users in final_suggestions_list:
+        username_profile.append(users.id)
+
+    for ids in username_profile:
+        profile_lists = Profile.objects.filter(id_user=ids)
+        username_profile_list.append(profile_lists)
+
+    suggestions_username_profile_list = list(chain(*username_profile_list))
+
     posts = Post.objects.all()
     return render(request, 'index.html', {
         'user_profile': user_profile,
-        'posts': feed_list
+        'posts': feed_list,
+        'suggestions_username_profile_list': suggestions_username_profile_list[:4]
     })
 
 
